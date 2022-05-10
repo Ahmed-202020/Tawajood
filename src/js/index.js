6523fac3195f92874f278af20991f7cd72a97c2b
Ahmed-202020 <ak812554@gmail.com>
2022-05-10 04:48:54 +0200

    the last

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,19 @@ $(function(){
         e.preventDefault();
         $(".navbar .navbar-nav .nav-link").removeClass("active") ; 
         $(this).addClass("active") ; 
-    })
+    }) ;
+    $(".navbar .navbar-nav .lang").on("click" , function(){
+        $(this).removeClass("active") ; 
+        if($(this).text()=== "العربية"){
+            console.log("yes") ; 
+            $("html").attr("dir" , "rtl") ; 
+            $("html").attr("lang" , "ar") ;
+            $(this).text("English") ;  
+        }else{
+            console.log("no") ; 
+            $("html").attr("dir" , "ltr") ; 
+            $("html").attr("lang" , "en") ;
+            $(this).text("العربية") ;  
+        }
+    });
 });
